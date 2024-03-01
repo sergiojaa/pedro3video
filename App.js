c5cb3e007fd2f76a39efddda9efe6214f851d433
sergiojaa <119433399+sergiojaa@users.noreply.github.com>
2024-03-01 15:09:26 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import styles from './App.module.css';
+//tenaru operatprs 14wutamde
+
+function App() {
+  const age = 19 ;
+  const isGreen = true
+  return (
+    <div className='App'>
+  //   {age >= 18 ? <h1>Over Age</h1> : <h1>under age</h1>}
+  //   <h1 style={{color: isGreen ? 'green' : 'red'}}>this has color {}</h1>
+  //   {isGreen && <button>this is button</button>}
+    
+  </div>
+  )
+  
+}
+
+
+
+export default App;
